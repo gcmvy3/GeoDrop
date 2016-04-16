a40e25eae8091b742456127eed336a3ac8d7c4c2
BobsCandyCanes <grantmatthews28@yahoo.com>
2016-04-16 16:03:54 -0500

    Working towards reading location from Google API

--- app/src/main/java/geodrop/geodrop/MainActivity.java
+++ app/src/main/java/geodrop/geodrop/MainActivity.java
@@ -1,13 +1,32 @@
 package geodrop.geodrop;
 
+import android.location.Location;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
+import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 
 public class MainActivity extends AppCompatActivity implements
-        GoogleApiClient.ConnectionCallbacks
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener
 {
+    protected static final String TAG = "MainActivity";
+
+    protected GoogleApiClient mGoogleApiClient;
+
+    // The most recent location of the user's phone
+    private Location mLastLocation;
+
+    // For displaying the user's location on the screen
+    protected String mLatitudeLabel;
+    protected String mLongitudeLabel;
+    protected TextView mLatitudeText;
+    protected TextView mLongitudeText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -15,38 +34,53 @@ public class MainActivity extends AppCompatActivity implements
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mLatitudeLabel = getResources().getString(R.string.latitude_label);
+        mLongitudeLabel = getResources().getString(R.string.longitude_label);
+        mLatitudeText = (TextView) findViewById((R.id.latitude_text));
+        mLongitudeText = (TextView) findViewById((R.id.longitude_text));
+
         // Create an instance of GoogleAPIClient to use for location tracking
         if (mGoogleApiClient == null)
         {
-            mGoogleApiClient = new GoogleApiClient.Builder(this)
-                    .addConnectionCallbacks(this)
-                    .addOnConnectionFailedListener(this)
-                    .addApi(LocationServices.API)
-                    .build();
+            buildGoogleApiClient();
         }
+    }
+
+    protected synchronized void buildGoogleApiClient()
+    {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API) // This line lets us use location services
+                .build();
     }
 
     @Override
     protected void onStart()
     {
+        super.onStart();
+
         // Connect to the Google servers for location tracking
         mGoogleApiClient.connect();
-        super.onStart();
     }
 
     @Override
     protected void onStop()
     {
-        // Disconnect from the google location servers
-        mGoogleApiClient.disconnect();
         super.onStop();
+
+        // Disconnect from the google location servers
+        if(mGoogleApiClient.isConnected())
+        {
+            mGoogleApiClient.disconnect();
+        }
     }
 
     @Override
     public void onConnected(Bundle connectionHint)
     {
-        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
-                mGoogleApiClient);
+        //Called when the GoogleApiClient connects
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
         if (mLastLocation != null) {
             mLatitudeText.setText(String.valueOf(mLastLocation.getLatitude()));
             mLongitudeText.setText(String.valueOf(mLastLocation.getLongitude()));
@@ -56,6 +90,13 @@ public class MainActivity extends AppCompatActivity implements
     @Override
     public void onConnectionSuspended(int i)
     {
+        Log.i(TAG, "Connection suspended, trying to reconnect");
+        mGoogleApiClient.connect(); // Attempts to reconnect to the API
+    }
 
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult)
+    {
+        Log.i(TAG, "Connection to Google API failed!");
     }
 }
