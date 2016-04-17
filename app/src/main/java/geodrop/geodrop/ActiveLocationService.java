592bfc8306a51e77c472e5c3d242bc2e1fabde0b
BobsCandyCanes <grantmatthews28@yahoo.com>
2016-04-17 00:36:36 -0500

    ActiveLocationService now successfully reads location and asks the server if there are nearby drops

--- app/src/main/java/geodrop/geodrop/ActiveLocationService.java
+++ app/src/main/java/geodrop/geodrop/ActiveLocationService.java
@@ -4,6 +4,8 @@ import android.app.IntentService;
 import android.app.Service;
 import android.content.Intent;
 import android.location.Location;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Binder;
 import android.os.Bundle;
 import android.os.HandlerThread;
@@ -19,6 +21,16 @@ import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.Random;
 
 // LocationService runs in the background and tracks the user's location
@@ -27,12 +39,16 @@ public class ActiveLocationService extends Service implements
         GoogleApiClient.OnConnectionFailedListener,
         LocationListener
 {
+    final String IP = "http://192.168.111.143:8080/geodrop-server/api/getifnearby?";
+
+    private URL url;
+
     // Preferred delay between location updates
-    public static final long UPDATE_INTERVAL_IN_MILLISECONDS = 5;
+    public static final long UPDATE_INTERVAL_IN_MS = 50;
 
     // Minimum delay between location updates
-    public static final long FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS =
-            UPDATE_INTERVAL_IN_MILLISECONDS / 2;
+    public static final long FASTEST_UPDATE_INTERVAL_IN_MS =
+                                UPDATE_INTERVAL_IN_MS / 2;
 
     // Used to communicate with the Google servers
     protected GoogleApiClient mGoogleApiClient;
@@ -42,6 +58,8 @@ public class ActiveLocationService extends Service implements
 
     // The most recent location of the user's phone
     private Location mCurrentLocation;
+
+    private boolean nearbyDrop = false;
 
     // Binder given to clients
     private final IBinder mBinder = new LocalBinder();
@@ -75,6 +93,8 @@ public class ActiveLocationService extends Service implements
     @Override
     public void onCreate()
     {
+        Log.i("Active Location Service", "Creating server thread!");
+
     }
 
     protected synchronized void buildGoogleApiClient()
@@ -90,8 +110,8 @@ public class ActiveLocationService extends Service implements
     protected void createLocationRequest()
     {
         mLocationRequest = new LocationRequest();
-        mLocationRequest.setInterval(UPDATE_INTERVAL_IN_MILLISECONDS);
-        mLocationRequest.setFastestInterval(FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS);
+        mLocationRequest.setInterval(UPDATE_INTERVAL_IN_MS);
+        mLocationRequest.setFastestInterval(FASTEST_UPDATE_INTERVAL_IN_MS);
         mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
     }
 
@@ -109,8 +129,7 @@ public class ActiveLocationService extends Service implements
             }
 
             startLocationUpdates();
-        }
-        catch(SecurityException e)
+        } catch (SecurityException e)
         {
             Log.i("Active Location Service", "Error: Location privilege not granted");
         }
@@ -133,17 +152,22 @@ public class ActiveLocationService extends Service implements
     public void onLocationChanged(Location location)
     {
         mCurrentLocation = location;
-        Log.i("Active Location Service", "Location changed!");
+
+        if(location != null)
+        {
+            // Ask the server whether there are any nearby drops
+            new ServerTask().execute(location.getLatitude(), location.getLongitude());
+        }
     }
 
     protected void startLocationUpdates()
     {
+        // Begin listening for changes in position
         try
         {
             LocationServices.FusedLocationApi.requestLocationUpdates(
                     mGoogleApiClient, mLocationRequest, this);
-        }
-        catch(SecurityException e)
+        } catch (SecurityException e)
         {
             Log.i("Active Location Service", "Error: location privileges not granted");
         }
@@ -152,14 +176,14 @@ public class ActiveLocationService extends Service implements
     @Override
     public int onStartCommand(Intent intent, int flags, int startId)
     {
-        return super.onStartCommand(intent,flags,startId);
+        return super.onStartCommand(intent, flags, startId);
     }
 
     @Override
     public void onDestroy()
     {
         // Disconnect from the google location servers
-        if(mGoogleApiClient.isConnected())
+        if (mGoogleApiClient.isConnected())
         {
             mGoogleApiClient.disconnect();
         }
@@ -169,4 +193,69 @@ public class ActiveLocationService extends Service implements
     {
         return mCurrentLocation;
     }
+
+    // This AsyncTask takes in a latitude and longitude and sends them to the server
+    // If there is a nearby drop, the server returns true
+    private class ServerTask extends AsyncTask<Double, Void, Boolean>
+    {
+        @Override
+        protected Boolean doInBackground(Double... params)
+        {
+            if (mCurrentLocation != null)
+            {
+                String urlString = "uninitialized";
+                try
+                {
+                    urlString = IP + "latitude=" + params[0] + "&" + "longitude=" + params[1];
+
+                    url = new URL(urlString);
+                }
+                catch (MalformedURLException e)
+                {
+                    Log.i("Active Location Service", "Invalid URL!");
+                }
+
+                URLConnection connection = null;
+                try
+                {
+                    connection = url.openConnection();
+
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream(), "UTF-8"));
+                    {
+                        String currentLine = null;
+                        while ((currentLine = reader.readLine()) != null)
+                        {
+                            if (currentLine.equals("true"))
+                            {
+                                return true;
+                            } else if (currentLine.equals("false"))
+                            {
+                                return false;
+                            }
+                        }
+                    }
+
+                } catch (IOException e)
+                {
+                    e.printStackTrace();
+                }
+
+                Log.i("Active Location Service", "Sending data to server!");
+            }
+            return false;
+        }
+
+        @Override
+        protected void onPostExecute(Boolean isNearbyDrop)
+        {
+            super.onPostExecute(isNearbyDrop);
+
+            if(isNearbyDrop)
+            {
+                System.out.println("NEARBY DROP!");
+            }
+        }
+    }
 }
+
+
--- app/src/main/java/geodrop/geodrop/DebugActivity.java
+++ app/src/main/java/geodrop/geodrop/DebugActivity.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
 import android.location.Location;
+import android.net.Uri;
 import android.os.Handler;
 import android.os.IBinder;
 import android.os.Looper;
@@ -13,9 +14,22 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
 
-public class DebugActivity extends AppCompatActivity
-{
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class DebugActivity extends AppCompatActivity {
     ActiveLocationService activeLocationService;
+
+    URL url;
 
     // Tracks whether the location service is bound
     private boolean mBound = false;
@@ -32,6 +46,11 @@ public class DebugActivity extends AppCompatActivity
 
     protected TextView mLatitudeLabelText;
     protected TextView mLongitudeLabelText;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,18 +68,22 @@ public class DebugActivity extends AppCompatActivity
         mLatitudeLabelText.setText(mLatitudeLabel);
         mLongitudeLabelText.setText(mLongitudeLabel);
 
-        locationUpdater = new Thread()
-        {
+        try {
+            url = new URL("http://172.16.100.110:8080/geodrop-server/api/");
+        } catch (MalformedURLException e) {
+            Log.i("DebugActivity", "Invalid URL!");
+        }
+
+        locationUpdater = new Thread() {
             Handler mainHandler = new Handler(Looper.getMainLooper());
 
-            public void run()
-            {
-                while(!Thread.currentThread().isInterrupted())
-                {
-                    try
-                    {
-                        if(mBound)
+            public void run() {
+                while (!Thread.currentThread().isInterrupted()) {
+                    try {
+                        if (mBound)
                         {
+                            // Update the location and the UI
+                            // UI update must be done with handler.post
                             mainHandler.post(new Runnable()
                             {
                                 @Override
@@ -72,61 +95,86 @@ public class DebugActivity extends AppCompatActivity
                             });
                         }
 
-                         Thread.sleep(500);
-                    }
-                    catch(Exception e)
-                    {
+                        Thread.sleep(500);
+                    } catch (Exception e) {
                     }
                 }
             }
         };
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     @Override
-    public void onStart()
-    {
+    public void onStart() {
         super.onStart();
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
 
         // Launch a location service and bind to it
         Intent intent = new Intent(this, ActiveLocationService.class);
         bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
 
-        if(!locationUpdater.isAlive())
-        {
+        if (!locationUpdater.isAlive()) {
             locationUpdater.start();
         }
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Debug Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://geodrop.geodrop/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
     }
 
     @Override
-    protected void onStop()
-    {
+    protected void onStop() {
         super.onStop();
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Debug Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://geodrop.geodrop/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
 
         // Unbind from the location service
-        if (mBound)
-        {
+        if (mBound) {
             unbindService(mConnection);
             mBound = false;
         }
 
         locationUpdater.interrupt();
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.disconnect();
     }
 
-    private void updateUI()
-    {
-        if(mCurrentLocation != null)
-        {
+    private void updateUI() {
+        if (mCurrentLocation != null) {
             mLatitudeText.setText(String.valueOf(mCurrentLocation.getLatitude()));
             mLongitudeText.setText(String.valueOf(mCurrentLocation.getLongitude()));
         }
     }
 
     // Used to communicate with the ActiveLocationService
-    private ServiceConnection mConnection = new ServiceConnection()
-    {
+    private ServiceConnection mConnection = new ServiceConnection() {
         @Override
-        public void onServiceConnected(ComponentName className, IBinder service)
-        {
+        public void onServiceConnected(ComponentName className, IBinder service) {
             // We've bound to LocalService, cast the IBinder and get LocalService instance
             ActiveLocationService.LocalBinder binder = (ActiveLocationService.LocalBinder) service;
             activeLocationService = binder.getService();
@@ -134,8 +182,7 @@ public class DebugActivity extends AppCompatActivity
         }
 
         @Override
-        public void onServiceDisconnected(ComponentName arg0)
-        {
+        public void onServiceDisconnected(ComponentName arg0) {
             mBound = false;
         }
     };
