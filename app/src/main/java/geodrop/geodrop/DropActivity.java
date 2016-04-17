a8e824dcabd08a0b2a1392245df32f0e658a9845
cylon56 <michaelnlewellen@gmail.com>
2016-04-17 01:05:10 -0500

    globe

---
+++ app/src/main/java/geodrop/geodrop/DropActivity.java
@@ -0,0 +1,13 @@
+package geodrop.geodrop;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DropActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drop);
+    }
+}
